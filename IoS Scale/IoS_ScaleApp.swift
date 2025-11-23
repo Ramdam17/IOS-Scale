051fd011bf579ffeecb865d8267954c85b9029d7
Ramdam17 <remy.ramadour.labs@gmail.com>
2025-11-23 02:50:35 -0500

    Initial Commit

---
+++ IoS Scale/IoS_ScaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IoS_ScaleApp.swift
+//  IoS Scale
+//
+//  Created by Remy Ramadour on 2025-11-23.
+//
+
+import SwiftUI
+
+@main
+struct IoS_ScaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
